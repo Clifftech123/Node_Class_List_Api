e8907d136b8bff12eaae0eac68c473da935c89fd
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2022-12-31 20:04:43 +0000

    Adding frist routing to get all members

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-import express from "express";
+import express, {Express, Request, Response} from 'express';
+import MembersRoute from './routes/Member.route'
+const app: Express = express();
 
 
-const app = express();
-// This is just a start
 
-app.get('/', (req, res) => {
-    res.send('Welcome the richApi !');
+// middleware the print  the url and the time it made the request
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta}ms`);
 })
 
+
+// Printing the port server in the console 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 })
+
+
+// Bae middleware 
+app.use(express.json());
+
+// Getting a single Member information  form the Api  
+app.use("/members", MembersRoute);
