35dd5ef1507f7739a66a16a41a1929ccd3d77307
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-01 11:48:05 +0000

    Adding of more mebers list

--- src/models/List.ts
+++ src/models/List.ts
@@ -6,8 +6,6 @@ interface Members{
     NetWorth: number
     Company: string
     Country:string
-    
-
 }
 
 const ListMembers: Members[] = [
@@ -42,6 +40,71 @@ const ListMembers: Members[] = [
         BillionaireName: 'Mark Zuckerberg',
         NetWorth: 84,
         Company: 'Facebook',
+        Country: 'USA'
+    },
+    {
+        Position: 5,
+        BillionaireName: 'Amancio Ortega',
+        NetWorth: 81,
+        Company: 'Inditex',
+        Country: 'Spain'
+    },
+    {
+        Position: 6,
+        BillionaireName: 'Carlos Slim Helu & family',
+        NetWorth: 72,
+        Company: 'America Movil',
+        Country: 'Mexico'
+    },
+    {
+        Position: 7,
+        BillionaireName: 'Sergey Brin',
+        NetWorth: 68,
+        Company: 'Google',
+        Country: 'USA'
+    },
+
+    {
+        Position: 8,
+        BillionaireName: 'Larry Page',
+        NetWorth: 67,
+        Company: 'Google',
+        Country: 'USA'
+    },
+
+    {
+        Position: 9,
+        BillionaireName: 'Mukesh Ambani',
+        NetWorth: 67,
+        Company: 'Reliance',
+        Country: 'India'
+    },
+    {
+        Position: 10,
+        BillionaireName: 'Steve Ballmer',
+        NetWorth: 67,
+        Company: 'Microsoft',
+        Country: 'USA'
+    },
+    {
+        Position: 11,
+        BillionaireName: 'Alice Walton',
+        NetWorth: 59,
+        Company: 'Walmart',
+        Country: 'USA'
+    },
+    {
+        Position: 12,
+        BillionaireName: 'Michael Bloomberg',
+        NetWorth: 58,
+        Company: 'Bloomberg',
+        Country: 'USA'
+    },
+    {
+        Position: 13,
+        BillionaireName: 'Jim Walton',
+        NetWorth: 57,
+        Company: 'Walmart',
         Country: 'USA'
     },
 ]
