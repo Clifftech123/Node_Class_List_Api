c7456b05628c5be203731e768415c48107c59d11
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-06 22:38:00 +0000

    Adding get all member route

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require( 'express' );
+import express from "express";
+import bodyParser from "body-parser";
+
+import membersRoute from "./routes/membersRoute.js";
 
 const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log( `Server listening on port ${PORT}` );
-});     
+app.listen(PORT, () =>
+	console.log(`Server running on port: http://localhost:${PORT}`)
+);
+
+// middleware  to parse the body of the request
+app.use( bodyParser.json() );
+
+// Get all the member in the array 
+app.use('/members', membersRoute);
