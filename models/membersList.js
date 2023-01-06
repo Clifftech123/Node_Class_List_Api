d90bfda8ed9b9443038ab3249189a0dcf9c30917
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-06 22:41:37 +0000

    Installing of nodemon package

---
+++ models/membersList.js
@@ -0,0 +1,34 @@
+export const Members = [
+	{
+		id: 1,
+		name: "Elon Musk",
+		Networth: "$219 B",
+		Age: "50",
+		Country: "United States",
+		Industry: "Technology",
+	},
+	{
+		id: 2,
+		name: "Elon Musk",
+		Networth: "$219 B",
+		Age: "50",
+		Country: "United States",
+		Industry: "Technology",
+	},
+	{
+		id: 3,
+		name: "Elon Musk",
+		Networth: "$219 B",
+		Age: "50",
+		Country: "United States",
+		Industry: "Technology",
+	},
+	{
+		id: 4,
+		name: "Elon Musk",
+		Networth: "$219 B",
+		Age: "50",
+		Country: "United States",
+		Industry: "Technology",
+	},
+];
