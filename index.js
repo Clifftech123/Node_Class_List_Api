e1f517cd9ab78b94b25faf43bc95451162c9d39e
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-09 10:54:51 +0000

    addding of two more route get single member and p;ost member

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import Moment from "moment/moment.js";
 import membersRoute from "./routes/membersRoute.js";
+
 
 const app = express();
 
-const PORT = 3000;
+// middleware the print  the url and the time it made the request
+app.use( ( req, res, next ) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta}ms`);
+})
 
+
+// middleware the print  the url and the time it made the request
+app.use( ( req, res, next ) => {
+	console.log(
+		`${req.protocol}://${req.get( "host" )}${req.originalUrl
+		} : ${Moment().format()}`
+	); // printing out the url that was hit
+	next();
+} );
+
+
+// port to run the server
+const PORT = 3000;
 app.listen(PORT, () =>
 	console.log(`Server running on port: http://localhost:${PORT}`)
 );
@@ -15,4 +35,6 @@ app.listen(PORT, () =>
 app.use( bodyParser.json() );
 
 // Get all the member in the array 
-app.use('/members', membersRoute);
+app.use( '/members', membersRoute )
+
+
