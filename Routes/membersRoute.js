2b48292b0b20aa44f6501660df85746635043295
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-09 10:52:25 +0000

    Adding of get single member and psot meber Route

--- Routes/membersRoute.js
+++ Routes/membersRoute.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { getMember } from ".././controllers/members.js";
+import { getMembers, getMember, postMember } from ".././controllers/members.js";
 
 const router = express.Router();
 // Get all members in the server
-router.get( "/", getMember );
+router.get( "/", getMembers );
+
+
+// Get a specific member in the server
+router.get( "/:id", getMember );
+
+// post member in the server
+
+router.post( "/", postMember );
+
+
 
 
 export default router;
+
+
