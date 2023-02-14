fe100ea6db71117cefcdd0d25036457389b999b2
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-02-14 23:05:04 +0000

    Adding of list

--- src/models/membersList.ts
+++ src/models/membersList.ts
@@ -31,7 +31,527 @@ const Members =   [
 		 Industry: "Fashion & Retail",
 		 Company: "LVMH"
 
-	 },
+	},
+	 
+	{
+		id: 4,
+		name: "Bill Gates",
+		Networth: "$124 B",
+		Age: 65,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Microsoft"
+	
+	},
+	
+	{
+		id: 5,
+		name: "Mark Zuckerberg",
+		Networth: "$97 B",
+		Age: 36,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Facebook"	
+
+	},
+
+	{
+		id: 6,
+		name: "Warren Buffett",
+		Networth: "$96 B",
+		Age: 90,
+		Country: "United States",
+		Industry: "Finance & Investments",
+		Company: "Berkshire Hathaway"
+
+	},
+
+	{
+		id: 7,
+		name: "Larry Ellison",
+		Networth: "$93 B",
+		Age: 76,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Oracle"
+	},
+
+	{
+		id: 8,
+		name: "Larry Page",
+		Networth: "$91 B",
+		Age: 48,
+		Country: "United States",
+
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 9,
+		name: "Mukesh Ambani",
+		Networth: "$84 B",
+		Age: 63,
+		Country: "India",
+		Industry: "Diversified",
+		Company: "Reliance Industries"
+
+	},
+
+	{
+		id: 10,
+		name: "Sergey Brin",
+		Networth: "$83 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 11,
+		name: "Steve Ballmer",
+		Networth: "$82 B",
+		Age: 64,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Microsoft"
+
+	},
+
+	{
+		id: 12,
+		name: "Charles Koch",
+		Networth: "$81 B",
+		Age: 85,
+		Country: "United States",
+		Industry: "Finance & Investments",
+		Company: "Koch Industries"
+
+	},
+
+	{
+		id: 13,
+		name: "David Koch",
+		Networth: "$81 B",
+		Age: 79,
+		Country: "United States",
+		Industry: "Finance & Investments",
+		Company: "Koch Industries"
+
+	},
+	{
+		id: 14,
+		name: "Amancio Ortega",
+		Networth: "$80 B",
+		Age: 85,
+		Country: "Spain",
+		Industry: "Fashion & Retail",
+		Company: "Inditex"
+
+	},
+
+
+	{
+		id: 15,
+		name: "Francoise Bettencourt Meyers",
+		Networth: "$80 B",
+		Age: 69,
+		Country: "France",
+		Industry: "Fashion & Retail",
+		Company: "L'Oreal"
+	},
+
+	{
+		id: 16,
+		name: "Jim Walton",
+		Networth: "$79 B",
+		Age: 74,
+		Country: "United States",
+		Industry: "Retail",
+		Company: "Walmart"
+	},
+
+	{
+		id: 17,
+		name: "Alice Walton",
+		Networth: "$79 B",
+		Age: 70,
+		Country: "United States",
+		Industry: "Retail",
+		Company: "Walmart"
+	},
+
+	{
+		id: 18,
+		name: "Rob Walton",
+		Networth: "$79 B",
+		Age: 71,
+		Country: "United States",
+		Industry: "Retail",
+		Company: "Walmart"
+
+	}
+	,
+	{
+		id: 19,
+		name: "Christy Walton",
+		Networth: "$79 B",
+		Age: 69,
+		Country: "United States",
+		Industry: "Retail",
+		Company: "Walmart"
+
+	},
+
+	{
+		id: 20,
+		name: "S. Robson Walton",
+		Networth: "$79 B",
+		Age: 72,
+		Country: "United States",
+		Industry: "Retail",
+		Company: "Walmart"
+	},
+
+	{
+		id: 21,
+		name: "Liliane Bettencourt",
+		Networth: "$78 B",
+		Age: 95,
+		Country: "France",
+		Industry: "Fashion & Retail",
+		Company: "L'Oreal"
+	},
+	{
+		id: 22,
+		name: "Jack Ma",
+		Networth: "$77 B",
+		Age: 56,
+		Country: "China",
+		Industry: "Technology",
+		Company: "Alibaba"
+	},
+
+	{
+		id: 23,
+		name: "Michael Bloomberg",
+		Networth: "$76 B",
+		Age: 78,
+		Country: "United States",
+		Industry: "Finance & Investments",
+		Company: "Bloomberg LP"
+
+	},
+
+	{
+		id: 24,
+		name: "Phil Knight",
+		Networth: "$75 B",
+		Age: 82,
+		Country: "United States",
+		Industry: "Fashion & Retail",
+		Company: "Nike"
+
+	},
+
+	{
+		id: 25,
+		name: "Jeff Bezos",
+		Networth: "$74 B",
+		Age: 57,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Amazon"
+
+	},
+
+	{
+		id: 26,
+		name: "Larry Page",
+		Networth: "$73 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+},
+
+	{
+		id: 27,
+		name: "Sergey Brin",
+		Networth: "$73 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 28,
+		name: "Mark Zuckerberg",
+		Networth: "$72 B",
+		Age: 36,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Facebook"
+
+	},
+
+	{
+
+		id: 29,	
+		name: "Warren Buffett",
+		Networth: "$71 B",
+		Age: 89,
+		Country: "United States",
+		Industry: "Finance & Investments",
+		Company: "Berkshire Hathaway"
+
+	},
+
+
+	{
+		id: 30,
+		name: "Larry Ellison",
+		Networth: "$70 B",
+		Age: 76,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Oracle"
+
+	},
+
+	{
+		id: 31,
+		name: "Larry Page",
+		Networth: "$69 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+	},
+
+
+	{
+		id: 32,
+		name: "Sergey Brin",
+		Networth: "$69 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 33,
+		name: "Larry Ellison",
+		Networth: "$68 B",
+		Age: 76,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Oracle"
+
+	},
+
+	{
+		id: 34,
+		name: "Larry Page",
+		Networth: "$67 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+		{
+			id: 35,
+			name: "Sergey Brin",
+			Networth: "$67 B",
+			Age: 47,
+			Country: "United States",
+			Industry: "Technology",
+			Company: "Alphabet"
+		
+	},
+		
+	{
+		id: 36,
+		name: "Larry Ellison",
+		Networth: "$66 B",
+		Age: 76,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Oracle"
+
+
+	},
+	
+	{
+		id: 37,
+		name: "Larry Page",
+		Networth: "$65 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+
+	{
+		id: 38,
+		name: "Sergey Brin",
+		Networth: "$65 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 39,
+		name: "Larry Ellison",
+		Networth: "$64 B",
+		Age: 76,
+		Country: "United States",
+
+		Industry: "Technology",
+		Company: "Oracle"
+	},
+
+	{
+		id: 40,
+		name: "Larry Page",
+		Networth: "$63 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+
+		id: 41,
+		name: "Sergey Brin",
+		Networth: "$63 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 42,
+		name: "Larry Ellison",
+		Networth: "$62 B",
+		Age: 76,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Oracle"	
+
+	},
+
+	{
+		id: 43,
+		name: "Larry Page",
+		Networth: "$61 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+		
+	},
+
+	{
+		id: 44,
+		name: "Sergey Brin",
+		Networth: "$61 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 45,
+		name: "Larry Ellison",
+		Networth: "$60 B",
+		Age: 76,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Oracle"
+
+	},
+
+	{
+		id: 46,
+		name: "Larry Page",
+		Networth: "$59 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+
+	},
+
+	{
+		id: 47,
+		name: "Sergey Brin",
+		Networth: "$59 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+
+	{
+		id: 48,
+		name: "Larry Ellison",
+		Networth: "$58 B",
+		Age: 76,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Oracle"
+
+	},
+
+
+	{
+		id: 49,
+		name: "Larry Page",
+		Networth: "$57 B",
+		Age: 48,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+
+	},
+
+	{
+		id: 50,
+		name: "Sergey Brin",
+		Networth: "$57 B",
+		Age: 47,
+		Country: "United States",
+		Industry: "Technology",
+		Company: "Alphabet"
+		
+}
+
+
+	
+
 ]  ;
 
 export default Members;
